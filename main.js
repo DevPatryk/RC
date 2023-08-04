ad7e43a55b85a33b7aee6168e385366d67c65822
unknown <pmatuszak1607@gmail.com>
2023-08-04 20:52:08 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+ScrollReveal({
+    reset:false,
+    distance:'600px',
+    duration:2500,
+    delay:400
+});
+
+ScrollReveal().reveal('.main-web-title',  {delay:500})
+ScrollReveal().reveal('.under-title-surname',  {delay:500})
+ScrollReveal().reveal('.header-description',  {delay:500})
+ScrollReveal().reveal('.section-title-about',  {delay:400})
+ScrollReveal().reveal('.image-and-text-box-owner',  {delay:400})
+ScrollReveal().reveal('.box',  {delay:400})
+
